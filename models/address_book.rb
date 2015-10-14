53de0024b7c4493c836379d1b6ad0bb933e75c78
Kyle Kwon <kyleskwon@gmail.com>
2015-10-14 13:33:15 -0700

    Added binary search algorithm

--- models/address_book.rb
+++ models/address_book.rb
@@ -23,6 +23,27 @@ class AddressBook
 
    end
 
+   # Search AddressBook for a specific entry by name
+   def binary_search(name)
+
+     lower = 0
+     upper = entries.length - 1
+
+     while lower <= upper
+       mid = (lower + upper) / 2
+       mid_name = entries[mid].name
+       if name == mid_name
+         return entries[mid]
+       elsif name < mid_name
+         upper = mid - 1
+       elsif name > mid_name
+         lower = mid + 1
+       end
+     end
+     
+     return nil
+   end
+
    def import_from_csv(file_name)
      csv_text = File.read(file_name)
      csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
